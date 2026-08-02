fe500a0845b42e75cff73360905e4a2a74546104
Eduardo <eduardo.olvreis@gmail.com>
2026-08-02 13:49:30 -0300

    feat: adiciona entidade Proposta

---
+++ RentalPipeline/Entities/Proposta.cs
@@ -0,0 +1,17 @@
+﻿using RentalPipeline.Entities.Enums;
+
+namespace RentalPipeline.Entities
+{
+    public class Proposta
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public Guid ImovelId { get; set; }
+        public Guid ClienteId { get; set; }
+        public StatusProposta Status { get; set; }
+        public DateTime CriadoEm { get; set; } = DateTime.UtcNow;
+        public DateTime AtualizadoEm { get; set; } = DateTime.UtcNow;
+
+        public Imovel Imovel { get; set; } = null!;
+        public Cliente Cliente { get; set; } = null!;
+    }
+}
