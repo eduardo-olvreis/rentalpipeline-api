60923758b8bc504c20a971d5e371e8b3c1364475
Eduardo <eduardo.olvreis@gmail.com>
2026-08-02 13:53:41 -0300

    feat: adiciona entidade HistoricoProposta

---
+++ RentalPipeline/Entities/HistoricoProposta.cs
@@ -0,0 +1,15 @@
+﻿using RentalPipeline.Entities.Enums;
+
+namespace RentalPipeline.Entities
+{
+    public class HistoricoProposta
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public Guid PropostaId { get; set; }
+        public StatusProposta StatusAnterior { get; set; }
+        public StatusProposta StatusNovo { get; set; }
+        public DateTime CriadoEm { get; set; }
+
+        public Proposta Proposta { get; set; } = null!;
+    }
+}
