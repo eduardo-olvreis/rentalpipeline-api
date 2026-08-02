79f68f8ab21aa932955fd07f37b5f69ac1f00fa8
Eduardo <eduardo.olvreis@gmail.com>
2026-08-02 13:38:32 -0300

    feat: adiciona Enums StatusImovel e StatusProposta

---
+++ RentalPipeline/Entities/Enums/StatusImovel.cs
@@ -0,0 +1,9 @@
+﻿namespace RentalPipeline.Entities.Enums
+{
+    public enum StatusImovel
+    {
+        Disponivel = 1,
+        EmNegociacao = 2,
+        Alugado = 3
+    }
+}
---
+++ RentalPipeline/Entities/Enums/StatusProposta.cs
@@ -0,0 +1,13 @@
+﻿namespace RentalPipeline.Entities.Enums
+{
+    public enum StatusProposta
+    {
+        Nova = 1,
+        AnaliseCredito = 2,
+        ContratoEmitido = 3,
+        Assinado = 4,
+        Ativo = 5,
+        Reprovada = 6,
+        Cnacelada = 7
+    }
+}
