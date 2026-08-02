65c275da124952416cb5980413c3b32ce67d2b90
Eduardo <eduardo.olvreis@gmail.com>
2026-08-02 15:17:40 -0300

    feat: adiciona interface da PropostaService

---
+++ RentalPipeline/Services/IPropostaService.cs
@@ -0,0 +1,11 @@
+﻿using RentalPipeline.DTOs.Propostas;
+
+namespace RentalPipeline.Services
+{
+    public interface IPropostaService
+    {
+        Task<PropostaResponseDto> CriarPropostaAsync(PropostaCreateDto propostaDto);
+        Task<PropostaResponseDto> AtualizarStatusAsync(Guid id, PropostaUpdateStatusDto propostaDto);
+        Task<IEnumerable<HistoricoPropostaResponseDto>> ObterHistoricoAsync(Guid id);
+    }
+}
