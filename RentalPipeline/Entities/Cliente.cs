f01cf63a326552c45c4ca838c2ce0abbd9ca9e6e
Eduardo <eduardo.olvreis@gmail.com>
2026-08-02 13:44:43 -0300

    feat: adiciona entidade Cliente

---
+++ RentalPipeline/Entities/Cliente.cs
@@ -0,0 +1,11 @@
+﻿namespace RentalPipeline.Entities
+{
+    public class Cliente
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public string Nome { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string Cpf {  get; set; } = string.Empty;
+        public DateTime CriadoEm { get; set; } = DateTime.Now;
+    }
+}
