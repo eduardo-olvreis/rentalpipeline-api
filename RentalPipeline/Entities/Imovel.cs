d4cc70aa1f784e5602f8065bd21cb6665fd49bcd
Eduardo <eduardo.olvreis@gmail.com>
2026-08-02 13:47:15 -0300

    feat: adiciona entidade Imovel

---
+++ RentalPipeline/Entities/Imovel.cs
@@ -0,0 +1,13 @@
+﻿using RentalPipeline.Entities.Enums;
+
+namespace RentalPipeline.Entities
+{
+    public class Imovel
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public string Endereco { get; set; } = string.Empty;
+        public decimal ValorAluguel { get; set; }
+        public StatusImovel Status { get; set; } = StatusImovel.Disponivel;
+        public DateTime CriadoEm { get; set; } = DateTime.UtcNow;
+    }
+}
