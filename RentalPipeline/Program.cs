85742947b9ff9c1b681a7edf85c04559547abfa0
Eduardo <eduardo.olvreis@gmail.com>
2026-08-02 14:21:04 -0300

    feat: adiciona Scalar e DbContext na program

--- RentalPipeline/Program.cs
+++ RentalPipeline/Program.cs
@@ -1,13 +1,28 @@
+using Microsoft.EntityFrameworkCore;
+using RentalPipeline.Data;
+using Scalar.AspNetCore;
+using System.Text.Json.Serialization;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
+builder.Services.AddDbContext<AppDbContext>(options =>
+   options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddControllers().AddJsonOptions(options => {
+        options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
+});
+builder.Services.AddOpenApi();
 
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.MapScalarApiReference(options =>
+    {
+        options.WithTitle("Rental Pipeline API - Teste Auxiliadroa Predial");
+    });
 }
 
 app.UseHttpsRedirection();
