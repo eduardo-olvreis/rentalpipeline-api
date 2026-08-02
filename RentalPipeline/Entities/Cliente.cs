153033e9dd2ba8b3b092ca6bbcb7fca2b24ea5f2
Eduardo <eduardo.olvreis@gmail.com>
2026-08-02 14:55:34 -0300

    fix: ajuste no DateTime da entidade Cliente para UtcNow

--- RentalPipeline/Entities/Cliente.cs
+++ RentalPipeline/Entities/Cliente.cs
@@ -6,6 +6,6 @@
         public string Nome { get; set; } = string.Empty;
         public string Email { get; set; } = string.Empty;
         public string Cpf {  get; set; } = string.Empty;
-        public DateTime CriadoEm { get; set; } = DateTime.Now;
+        public DateTime CriadoEm { get; set; } = DateTime.UtcNow;
     }
 }
