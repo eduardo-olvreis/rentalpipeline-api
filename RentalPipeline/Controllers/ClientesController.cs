0e7ba22802774b269984a8aea01eee339daed06c
Eduardo <eduardo.olvreis@gmail.com>
2026-08-02 18:59:27 -0300

    feat: adiciona ClientesController

---
+++ RentalPipeline/Controllers/ClientesController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RentalPipeline.DTOs.Clientes;
+using RentalPipeline.Services;
+
+namespace RentalPipeline.Controllers
+{
+    [ApiController]
+    [Route("apo/[controller]")]
+    public class ClientesController : ControllerBase
+    {
+        private readonly IClienteService _clienteService;
+        public ClientesController(IClienteService clienteService)
+        {
+            _clienteService = clienteService;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<ClienteResponseDto>>> ObterTodos()
+        {
+            var clientes = await _clienteService.ObterTodosAsync();
+            return Ok(clientes);
+        }
+
+        [HttpGet("{id:guid}")]
+        public async Task<ActionResult<ClienteResponseDto>> ObterPorId(Guid id)
+        {
+            var cliente = await _clienteService.ObterPorIdAsync(id);
+            return Ok(cliente);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<ClienteResponseDto>> Criar([FromBody] ClienteCreateDto dto)
+        {
+            var cliente = await _clienteService.CriarAsync(dto);
+            return CreatedAtAction(nameof(ObterPorId), new { id = cliente.Id }, cliente);
+        }
+    }
+
+}
