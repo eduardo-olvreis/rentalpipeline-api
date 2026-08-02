94f494881163ec5ab591a6a6d29fa18cefd6a891
Eduardo <eduardo.olvreis@gmail.com>
2026-08-02 19:00:39 -0300

    feat: adiciona ImoveisController

---
+++ RentalPipeline/Controllers/ImoveisController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RentalPipeline.DTOs.Imoveis;
+using RentalPipeline.Services;
+
+namespace RentalPipeline.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ImoveisController : ControllerBase
+    {
+        private readonly IImovelService _imovelService;
+        public ImoveisController(IImovelService imovelService)
+        {
+            _imovelService = imovelService;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<ImovelResponseDto>>> ObterTodos()
+        {
+            var imoveis = await _imovelService.ObterTodosAsync();
+            return Ok(imoveis);
+        }
+
+        [HttpGet("{id:guid}")]
+        public async Task<ActionResult<ImovelResponseDto>> ObterPorId(Guid id)
+        {
+            var imovel = await _imovelService.ObterPorIdAsync(id);
+            return Ok(imovel);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<ImovelResponseDto>> Criar([FromBody] ImovelCreateDto dto)
+        {
+            var imovel = await _imovelService.CriarAsync(dto);
+            return CreatedAtAction(nameof(ObterPorId), new { id = imovel.Id }, imovel);
+        }
+    }
+}
