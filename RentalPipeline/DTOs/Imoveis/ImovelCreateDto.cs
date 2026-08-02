bb3282211b55c9af1acc1f62c7c1c12ecb428a80
Eduardo <eduardo.olvreis@gmail.com>
2026-08-02 14:59:32 -0300

    feat: adiciona Imoveis Dtos

---
+++ RentalPipeline/DTOs/Imoveis/ImovelCreateDto.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RentalPipeline.DTOs.Imoveis
+{
+    public class ImovelCreateDto
+    {
+        [Required(ErrorMessage = "O endereço é obrigatório.")]
+        public string Endereco { get; set; } = string.Empty;
+
+        [Range(0.01, double.MaxValue, ErrorMessage = "O valor do aluguel deve ser maior que zero.")]
+        public decimal ValorAluguel { get; set; }
+    }
+}
---
+++ RentalPipeline/DTOs/Imoveis/ImovelResponseDto.cs
@@ -0,0 +1,13 @@
+﻿using RentalPipeline.Entities.Enums;
+
+namespace RentalPipeline.DTOs.Imoveis
+{
+    public class ImovelResponseDto
+    {
+        public Guid Id { get; set; }
+        public string Endereco { get; set; } = string.Empty;
+        public decimal ValorAluguel { get; set; }
+        public StatusImovel Status { get; set; }
+        public DateTime CriadoEm { get; set; }
+    }
+}
